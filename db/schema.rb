567e0b4cb76d23cb856bc1d2a977ee973506d44b
Donald Wu <yeukfei02@gmail.com>
2021-07-04 14:23:56 +0800

    feat: 🎸 add foreign key to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_114444) do
+ActiveRecord::Schema.define(version: 2021_07_04_052721) do
 
   create_table "cities", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "city_name"
     t.string "area"
-    t.integer "zone_id"
+    t.bigint "zone_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["zone_id"], name: "fk_rails_f8d2792419"
   end
 
   create_table "machines", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "machine_name"
     t.integer "serial_number"
-    t.integer "city_id"
+    t.bigint "city_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["city_id"], name: "fk_rails_d74b7fc4ce"
   end
 
   create_table "products", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
@@ -33,10 +35,12 @@ ActiveRecord::Schema.define(version: 2021_07_03_114444) do
     t.string "product_description"
     t.float "price"
     t.integer "quantity"
-    t.integer "machine_id"
-    t.integer "city_id"
+    t.bigint "machine_id"
+    t.bigint "city_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["city_id"], name: "fk_rails_391685e1f6"
+    t.index ["machine_id"], name: "fk_rails_87758910ea"
   end
 
   create_table "users", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
@@ -52,4 +56,8 @@ ActiveRecord::Schema.define(version: 2021_07_03_114444) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cities", "zones"
+  add_foreign_key "machines", "cities"
+  add_foreign_key "products", "cities"
+  add_foreign_key "products", "machines"
 end
