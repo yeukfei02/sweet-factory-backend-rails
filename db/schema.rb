d8f0b61323c3c51cbf19d7740f1b60a4eb3d8885
Donald Wu <yeukfei02@gmail.com>
2021-07-10 19:56:57 +0800

    fix: 🐛 update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_07_04_052721) do
 
-  create_table "cities", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "city_name"
     t.string "area"
     t.bigint "zone_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_052721) do
     t.index ["zone_id"], name: "fk_rails_f8d2792419"
   end
 
-  create_table "machines", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "machines", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "machine_name"
     t.integer "serial_number"
     t.bigint "city_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_052721) do
     t.index ["city_id"], name: "fk_rails_d74b7fc4ce"
   end
 
-  create_table "products", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "product_name"
     t.string "product_description"
     t.float "price"
@@ -43,14 +43,14 @@ ActiveRecord::Schema.define(version: 2021_07_04_052721) do
     t.index ["machine_id"], name: "fk_rails_87758910ea"
   end
 
-  create_table "users", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "zones", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "zones", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "zone_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
