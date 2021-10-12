602b0559941fd1cc218103f4106bf849080faf83
Donald Wu <yeukfei02@gmail.com>
2021-10-12 22:37:48 +0800

    feat: 🎸 add annotate lib

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string(255)
+#  password   :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
