43a4e3e01df12ff15a8254125968e54855b686e4
Donald Wu <yeukfei02@gmail.com>
2021-11-09 11:15:06 +0800

    fix: 🐛 update rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'support/factory_bot'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -26,6 +25,10 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |file| require file }
+Dir[Rails.root.join('spec', 'factories', '**', '*.rb')].sort.each { |file| require file }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
